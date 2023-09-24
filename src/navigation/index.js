3b804e81afa61cd2853ceb332923d686e7964a56
Holat <105239299+Holat@users.noreply.github.com>
2023-09-24 18:33:02 +0100

    navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
+import RecipeDetailsScreen from "../screens/RecipeDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +17,7 @@ function AppNavigator() {
       >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="RecipeDetail" component={RecipeDetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
